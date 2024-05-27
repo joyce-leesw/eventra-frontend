2a715867d44b4cc657b771a0f34fb498b74efc4d
harrywiner <winer.harry@gmail.com>
2024-05-27 14:54:07 +0100

    added loading fallback

--- src/App.tsx
+++ src/App.tsx
@@ -24,12 +24,14 @@ const App: React.FC = () => {
   const [events, setEvents] = useState<Event[]>([]);
   const [currentEventIndex, setCurrentEventIndex] = useState<number>(0);
   const [preferences, setPreferences] = useState<Preference[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchEvents = async () => {
       try {
         const response = await axios.get('http://127.0.0.1:8000/events/');
         setEvents(response.data.events);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching events:', error);
       }
@@ -40,10 +42,9 @@ const App: React.FC = () => {
 
   useEffect(() => {
     if (currentEventIndex === events.length) {
-      console.log('currentEventIndex is equal to the length of events');
       console.log('preferences: ', JSON.stringify(preferences));
     }
-  }, [currentEventIndex, events]);
+  }, [currentEventIndex, events, preferences]);
 
   const handleLikeClick = () => {
     setPreferences((prevPreferences: Preference[]) => [
@@ -61,25 +62,33 @@ const App: React.FC = () => {
     setCurrentEventIndex(prevIndex => prevIndex + 1);
   };
 
+
+  if (loading) {
+    return (
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="card flashcard-container">
       {events.length > 0 && currentEventIndex < events.length && (
-        <div className="card flashcard-container">
           <EventDetails 
             event={events[currentEventIndex]}
             onLikeClick={handleLikeClick}
             onDislikeClick={handleDislikeClick} 
           />
-        </div>
       )}
-      { currentEventIndex === events.length && (
-        <div className="card flashcard-container">
+      { currentEventIndex === events.length && events.length !== 0 && (
           <div className="card-body">
             <h5 className="card-title">Thank you for registering your preferences</h5>
           </div>
-        </div>
-      
       )}
+      </div>
     </div>
   );
 }
